686da27056b6e211cbb93379459160db6cea7185
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2024-10-03 10:17:04 -0500

    fix(config): switches to semantic plugins

--- src/semantic-release/index.js
+++ src/semantic-release/index.js
@@ -8,19 +8,16 @@ module.exports = {
     },
   ],
   plugins: [
-    '@semantic-release/changelog',
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
+    '@semantic-release/changelog',
     [
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-        branch: 'release',
         // eslint-disable-next-line no-template-curly-in-string -- required by `semantic-release`.
         message: 'chore(release): ${nextRelease.version} [skip ci]',
       },
     ],
-    '@semantic-release/npm',
   ],
 };
