b0e28d5c4e0884167b8c8c0311a848796d3a3c5a
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2024-10-09 18:37:27 -0500

    fix(config): renames vitest configs to typescript

--- src/vitest/setup.react.js
+++
@@ -1,5 +0,0 @@
-import { expect } from 'vitest';
-import 'vitest-axe/extend-expect';
-import * as matchers from 'vitest-axe/matchers';
-
-expect.extend(matchers);
--- src/vitest/vite.react.ts
+++ src/vitest/vite.react.ts
@@ -7,12 +7,13 @@ export default defineConfig({
   plugins: [react()],
   test: {
     coverage: {
+      // @ts-expect-error since its 'c8' custom
       provider: 'c8',
       reporter: ['text', 'html'],
     },
     css: true,
     environment: 'jsdom',
     globals: true,
-    setupFiles: require.resolve('./setup.react.js'),
+    setupFiles: require.resolve('./setup.react.ts'),
   },
 });
