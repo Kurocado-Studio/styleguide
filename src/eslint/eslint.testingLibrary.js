ab69a2d8491c5dda3d923361bdd39196faedd53d
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2024-10-19 18:44:14 -0500

    fix(config): fixes release workflow

--- src/eslint/eslint.testingLibrary.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    // We may want to separate this out in future, as it's only needed when
-    // using React with Testing Library.
-    'plugin:testing-library/react',
-  ],
-  plugins: ['testing-library'],
-};
