53329cad383a81a0e3d9b4f520582db06c8bbe1f
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2024-10-23 17:58:16 -0500

    feat(config): alpha release start (#65)

--- src/semantic-release/base.js
+++ src/semantic-release/base.js
@@ -2,9 +2,24 @@ module.exports = {
   branches: [
     'main',
     {
+      channel: 'alpha',
+      name: 'alpha',
+      prerelease: true,
+    },
+    {
+      channel: 'beta',
+      name: 'beta',
+      prerelease: true,
+    },
+    {
       channel: 'canary',
       name: 'canary',
       prerelease: true,
+    },
+    {
+      channel: 'pre/rc',
+      name: 'pre/rc',
+      prerelease: 'rc',
     },
   ],
   plugins: [
