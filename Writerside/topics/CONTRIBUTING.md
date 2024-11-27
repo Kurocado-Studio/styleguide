1589ef9108092d9a8e29dca48b08961af6cc67f8
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2024-11-27 11:53:22 -0600

    chore(config): adds copyright to files

--- Writerside/topics/CONTRIBUTING.md
+++ Writerside/topics/CONTRIBUTING.md
@@ -1,3 +1,12 @@
+<!--
+ * Made with ❤️ and adobo by Kurocado Studio
+ * Copyright (c) 2024. All Rights Reserved.
+ *
+ * Learn more about Kurocado Studio: {@link https://www.kurocado.studio}
+ *
+ * Explore our open-source projects: {@link https://github.com/kurocado-studio}
+-->
+
 # Contributing
 
 Thank you for your interest in contributing! We appreciate your efforts to help improve and maintain
--- src/vite/setup.web.js
+++ src/vite/setup.web.js
@@ -1,3 +1,12 @@
+/**
+ * Made with ❤️ and adobo by Kurocado Studio
+ * Copyright (c) 2024. All Rights Reserved.
+ *
+ * Learn more about Kurocado Studio: {@link https://www.kurocado.studio}
+ *
+ * Explore our open-source projects: {@link https://github.com/kurocado-studio}
+ */
+
 /* eslint @typescript-eslint/no-unsafe-member-access: 0 */
 import '@testing-library/jest-dom/vitest';
 import { config } from 'dotenv';
