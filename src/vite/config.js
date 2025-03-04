abd0e0b2aabea85740ac91614a84b5a81b8ec107
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-03-04 01:05:47 -0600

    fix(config): adds tsconfigPaths

--- src/vite/config.js
+++ src/vite/config.js
@@ -6,9 +6,10 @@
  *
  * Explore our open-source projects: {@link https://github.com/kurocado-studio}
  */
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // eslint-disable-next-line tsdoc/syntax
 /** @type {import('vite').Plugin[]} */
 export const viteConfig = {
-  plugins: [],
+  plugins: [tsconfigPaths()],
 };
